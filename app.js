02941a75f8e7709894dc9e9a1a3caf973f390cd3
Djibril-6et <djibril.6et@gmail.com>
2023-03-30 16:46:43 +0200

    list folders and pdfs

--- app.js
+++ app.js
@@ -7,23 +7,41 @@ const PORT = process.env.PORT || 7001;
 
 app.use('/pdf-files', express.static('pdf-files'));
 
-app.get('/pdfs', (req, res) => {
+app.get('/pdfs/:subfolder?', (req, res) => {
   const pdfDirectory = './pdf-files';
 
-  fs.readdir(pdfDirectory, (err, files) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Erreur lors de la lecture du dossier');
-      return;
-    }
+  const subfolder = req.params.subfolder || '';
 
-    const pdfFiles = files.filter(file => path.extname(file) === '.pdf');
-    const pdfUrls = pdfFiles.map(file => `http://${req.headers.host}/pdf-files/${file}`);
+  if (subfolder) {
+    const pdfUrls = [];
 
-    res.json(pdfUrls);
-  });
+    fs.readdir(path.join(pdfDirectory, subfolder), (err, files) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Erreur lors de la lecture du dossier');
+        return;
+      }
+
+      const pdfFiles = files.filter(file => path.extname(file) === '.pdf');
+      const pdfUrls = pdfFiles.map(file => `http://${req.headers.host}/pdf-files/${subfolder}/${file}`);
+
+      res.json(pdfUrls);
+    });
+  } else {
+    fs.readdir(pdfDirectory, (err, files) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Erreur lors de la lecture du dossier');
+        return;
+      }
+
+      const subfolders = files.filter(file => fs.lstatSync(path.join(pdfDirectory, file)).isDirectory());
+
+      res.json(subfolders);
+    });
+  }
 });
 
 app.listen(PORT, () => {
   console.log(`Serveur en écoute sur le port ${PORT}`);
-}); 
+});
